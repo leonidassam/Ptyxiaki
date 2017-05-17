28b4ea0515b7454e7fa917f177353aab01c7fb19
leonidassam <leonidas_zans@hotmail.com>
2017-05-17 10:26:43 +0300

    Add files via upload
    Layers

--- asdf.R
+++ asdf.R
@@ -1,82 +1,51 @@
 library( igraph )
-source( "PCI.R")
+source( "Functions.R")
 
 londonData <- read.table( './London_Multiplex_Transport/Dataset/london_transport_multiplex.edges', header = FALSE )
-position1 <- 0
-position2 <- 0
-position3 <- 0
 
-findPositions <- function( data) {
-  pos1 <- 0
-  pos2 <- 0
-  pos3 <- 0
-  for( i in data[,1]) {
-    
-    if(i==1){ 
-      pos1 <- pos1 + 1
-    }
-    if(i==2){ 
-      pos2 <- pos2 + 1
-    }
-    if(i==3){ 
-      pos3 <- pos3 + 1
-    }
-  }
-  
-  return( list(pos1, pos2, pos3))
-}
 
 position1 <- findPositions( londonData)[[1]]
 position2 <- findPositions( londonData)[[2]]
 position3 <- findPositions( londonData)[[3]]
 
 
+
 firstLayer <- londonData[ 1:position1, ]
 firstLayerEdgelist <- firstLayer[,2:3]
 firstLayerEdgelist <- firstLayerEdgelist[,] + 1
 firstLayerWeights <- firstLayer[,4]
+firstLayerGraph <- graph_from_edgelist( data.matrix( firstLayerEdgelist), directed = TRUE )
 
 secondLayer <- londonData[ (position1 + 1):( position1 + position2),]
 secondLayerEdgelist <- secondLayer[,2:3]
 secondLayerEdgelist <- secondLayerEdgelist[,] + 1
 secondLayrWeights <- secondLayer[,4]
+secondLayerGraph <- graph_from_edgelist( data.matrix( secondLayerEdgelist), directed = TRUE )
 
 thirdLayer <- londonData[ (position1 + position2 + 1):(position1 + position2 +position3),]
 thirdLayerEdgelist <- thirdLayer[,2:3]
 thirdLayerEdgelist <- thirdLayerEdgelist[,] + 1
 thirdLayerWeights <- thirdLayer[,4]
-
-
-
-firstLayerGraph <- graph_from_edgelist( data.matrix( firstLayerEdgelist), directed = TRUE )
-secondLayerGraph <- graph_from_edgelist( data.matrix( secondLayerEdgelist), directed = TRUE )
 thirdLayerGraph <- graph_from_edgelist( data.matrix( thirdLayerEdgelist), directed = TRUE )
-
 
 londonDataEdgeList <- londonData[,2:3]
 weight <- londonData[,4]
-
 londonDataEdgeList <- londonDataEdgeList[,] + 1
-matrixLondonDataEdgeList <- data.matrix( londonDataEdgeList)
-g <- graph_from_edgelist( matrixLondonDataEdgeList, directed = TRUE)
-
+g <- graph_from_edgelist( data.matrix( londonDataEdgeList), directed = TRUE)
 E(g)$weights <- weight
-#plot( g, layout = layout_( g, as_star()), edge.label = E(g)$weights)
 
 
 
+degrees <- data.frame( degree( firstLayerGraph, v = V(firstLayerGraph), mode = c( "all"), loops = FALSE, normalized = FALSE), ncol = 1)
+sortedDegrees <- data.frame( order( degrees, decreasing = TRUE))
+
+# plots
 if( FALSE) {
-	data <- read.csv( 'Network.csv', header = FALSE )
-	g <- graph.data.frame( data)
-
-	E(g)$weight=as.numeric( data[,3])
-	E(g)$curved <- 0.2
-	adjacencyMatrix <- get.adjacency( g, attr = "weight")
-
-
-
-	PCI <- calculatePCI()
-
-	print( PCI)
-	plot( g, edge.label = E(g)$weight)
+  
+  pdf( "Layers.pdf")
+  plot( firstLayerGraph, vertex.size = 0.1, edge.label = E( firstLayerGraph)$firstLayerWeights)
+  dev.off()
+  pdf( "London.pdf")
+  plot( g, vertex.size = 0.1, edge.label = E(g)$weights)
+  dev.off()
 }
